a4c196981eac2240d2a18e1505f792f45d43744a
unknown <Jose.Cordero@BARLTW1047.midasplayer.local>
2016-07-06 16:27:04 +0200

    Massive refactoring. Code might be broken

--- experiments/chosing_one_tag/source/_stupid_tools_and_helpers_scripting.py
+++ experiments/chosing_one_tag/source/_stupid_tools_and_helpers_scripting.py
@@ -18,6 +18,6 @@ def _aux_getNumberOfCasses(filename):
 
 def _aux_getSnapshotToBeused(classifier_name):
     snapshot_prefix_looked_for = '%s_snapshot_stage_2' % classifier_name
-    model_file='data/snapshots/' + sorted([(int(x.split(".")[0]), name) for x,name in [(x.split("_")[-1],x) for x in os.listdir('data/snapshots') if 'caffemodel' in x and snapshot_prefix_looked_for in x]], key = lambda x: x[0], reverse = True)[0][1]
+    model_file=settings['SNAPSHOT_PREFIX'] + "/" + sorted([(int(x.split(".")[0]), name) for x,name in [(x.split("_")[-1],x) for x in os.listdir(settings['SNAPSHOT_PREFIX']) if 'caffemodel' in x and snapshot_prefix_looked_for in x]], key = lambda x: x[0], reverse = True)[0][1]
     return model_file
 
