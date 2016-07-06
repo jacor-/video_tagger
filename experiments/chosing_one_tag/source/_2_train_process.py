20a49e4b97fd6efe77437b90df9fbc1d10a2268c
unknown <Jose.Cordero@BARLTW1047.midasplayer.local>
2016-07-06 14:48:51 +0200

    Fixing refactoring mess

--- experiments/chosing_one_tag/source/_2_train_process.py
+++ experiments/chosing_one_tag/source/_2_train_process.py
@@ -76,7 +76,7 @@ def trainNetworkFromScratch(CLASSIFIER_NAME, OUTPUT_CLASSES, VAL_FILENAME, TRAIN
     ###------------------ 2st stage ----------------------###
     #########################################################
     snapshot_prefix_looked_for = '%s_snapshot_stage_1' % CLASSIFIER_NAME
-    last_snapshot='data/snapshots/' + sorted([(int(x.split(".")[0]), name) for x,name in [(x.split("_")[-1],x) for x in os.listdir('data/snapshots') if 'caffemodel' in x and snapshot_prefix_looked_for in x]], key = lambda x: x[0], reverse = True)[0][1]
+    last_snapshot=settings['SNAPSHOT_PREFIX'] + "/" + sorted([(int(x.split(".")[0]), name) for x,name in [(x.split("_")[-1],x) for x in os.listdir('data/snapshots') if 'caffemodel' in x and snapshot_prefix_looked_for in x]], key = lambda x: x[0], reverse = True)[0][1]
 
     variables_to_replace = {
         'LRMULTBASENET' : '1',
